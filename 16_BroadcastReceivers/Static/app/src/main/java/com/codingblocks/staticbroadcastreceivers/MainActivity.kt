66b557d887f9b4c841473546478fcb9ffec48cfc
Naman Gupta <56741378+namangpt2312@users.noreply.github.com>
2020-12-10 04:35:36 +0530

    Update MainActivity.kt

--- 16_BroadcastReceivers/Static/app/src/main/java/com/codingblocks/staticbroadcastreceivers/MainActivity.kt
+++ 16_BroadcastReceivers/Static/app/src/main/java/com/codingblocks/staticbroadcastreceivers/MainActivity.kt
@@ -8,5 +8,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val psr = PowerStateReceiver()
+        val iFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+            addAction(Intent.ACTION_POWER_CONNECTED)
+        }
+        
+        registerReceiver(psr, iFilter)
     }
+    
+    inner class PowerStateReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if(intent == null) return;
+            
+            if(intent.action == Intent.ACTION_POWER_CONNECTED) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "CHARGER CONNECTED",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            
+            if(intent.action == Intent.ACTION_POWER_DISCONNECTED) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "CHARGER DISCONNECTED",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+        
 }
